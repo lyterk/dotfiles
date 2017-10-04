6e0aa9555cd966af9b754a3994f84071fdb74641
Kevin Lyter <kev@lyterk.com>
2017-10-03 21:46:51 -0700

    Dummy text

--- copier.sh
+++ copier.sh
@@ -1,13 +1,22 @@
-*** /tmp/ediff27163ZZd	2017-10-03 21:44:22.283430129 -0700
---- /tmp/ediff27163mjj	2017-10-03 21:44:22.283430129 -0700
-***************
-*** 15,18 ****
-      fi
-  done
-  
-! # also disagree here
---- 15,18 ----
-      fi
-  done
-  
-! # This is different too
+#!/bin/zsh
+
+echo "############################"
+echo "########### HOME ###########"
+echo "############################"
+
+cd $dir
+
+for file in $dir/.*; do
+    base=$(basename $file)
+    if [[ "$base" != ".git" ]]; then
+           echo $base
+           rm ~/$base
+           ln -s $file ~/$basename
+    fi
+done
+
+# This was the thing on the master branch
+
+# This was the thing on the master branch too
+
+# Third thing on the master branch
