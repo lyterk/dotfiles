3b95195e89eeec333a56144c47d28f113c12aa12
Kevin Lyter <kev@lyterk.com>
2017-10-03 22:02:39 -0700

    Making things cleaner and symmetrical

--- home/copier.sh
+++ home/copier.sh
@@ -1,18 +1,16 @@
 #!/bin/zsh
 
 echo "############################"
-echo "########### WORK ###########"
+echo "########### HOME ###########"
 echo "############################"
 
-dir=$HOME/dotfiles/work
-
-cd $dir
+dir=$HOME/dotfiles/home
 
 for file in $dir/*; do
     base=$(basename $file)
     if [[ "$base" != "copier.sh" ]]; then
-        echo $base
+        echo .$base
         rm $HOME/.$base
-        ln -s $file $HOME/.$base
+        ln -s $file ~/.$base
     fi
 done
