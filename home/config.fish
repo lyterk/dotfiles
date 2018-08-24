6d1ba0164b8b41afe758436fa94187ff4d9b0e8f
Kevin Lyter <lyterk@sent.com>
2018-08-24 00:05:51 -0700

    8/26

--- home/config.fish
+++ home/config.fish
@@ -1,5 +1,8 @@
 bind \e\b backward-kill-word
 
+alias pp "pass git push origin master"
+alias pl "pass git pull origin master"
+
 alias spac "sudo pacman"
 alias spacs "spac -S"
 alias sagi "spacs"
@@ -21,10 +24,11 @@ alias na "ls"
 set -gx BROWSER /usr/bin/firefox
 set -gx EDITOR "/usr/bin/emacsclient -t"
 set -gx GOPATH $HOME/.local/go
+set -gx LOCALBIN $HOME/.local/bin
 
 set RASP_GCC /usr/local/bin/aarch64-none-elf/bin
 set NPM_HOME /home/kev/.npm-packages/bin
 set CARGO $HOME/.cargo/bin
 set BASE_PATH /usr/local/bin /usr/bin /bin /usr/local/sbin /usr/lib/jvm/default/bin /usr/bin/site_perl /usr/bin/vendor_perl /usr/bin/core_perl
 
-set -gx PATH $BASE_PATH $RASP_GCC $NPM_HOME $CARGO $GOPATH/bin
+set -gx PATH $BASE_PATH $RASP_GCC $NPM_HOME $CARGO $GOPATH/bin $LOCALBIN
