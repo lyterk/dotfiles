6395778ba7243b15f00f3c381c653fc319b05979
Kevin Lyter <lyterk@sent.com>
2019-05-20 22:10:03 -0700

    Shell changes

--- home/config.fish
+++ home/config.fish
@@ -16,9 +16,9 @@ alias xcop "xclip -selection clipboard"
 alias xpa "xclip -selection clipboard -o"
 
 alias et "emacsclient -t"
-alias ed "emacs --daemon > ~/.emacs.d/logs/(date '+%m-%d-%y').log &"
+alias ed "emacs --daemon > $HOME/.emacs.d/logs/(date '+%m-%d-%y').log &"
 
-alias sz "source ~/.zshrc"
+alias sz "source $HOME/.zshrc"
 alias ls exa
 alias na "ls"
 
@@ -32,18 +32,22 @@ alias vsta "sudo systemctl start openvpn-client@client.service"
 set -x GTK_IM_MODULE "fcitx"
 set -x QT_IM_MODULE "fcitx"
 set -x XMODIFIERS "@im=fcitx"
-set -gx GOPATH ~/code/golang
-set -gx DOOMPATH ~/.emacs.d/bin
+set -gx GOPATH $HOME/code/golang
+set -gx DART_PATH $HOME/.pub-cache/bin
 
 set -gx BROWSER /usr/bin/firefox
 set -gx EDITOR "/usr/bin/emacsclient -t"
 set -gx GOBIN $GOPATH/bin
 set -gx LOCALBIN $HOME/.local/bin
 
-set EMACS_PATH /home/kev/.emacs.d/bin
+set FLUTTER_PATH $HOME/.local/share/flutter/bin
 set RASP_GCC /usr/local/bin/aarch64-none-elf/bin
-set NPM_HOME /home/kev/.npm-packages/bin
+set NPM_HOME $HOME/.npm-packages/bin
 set CARGO $HOME/.cargo/bin
 set BASE_PATH /usr/local/bin /usr/bin /bin /usr/local/sbin /usr/lib/jvm/default/bin /usr/bin/site_perl /usr/bin/vendor_perl /usr/bin/core_perl
 
-set -gx PATH $BASE_PATH $EMACS_PATH $RASP_GCC $NPM_HOME $CARGO $GOPATH/bin $LOCALBIN $GOBIN $DOOMPATH
+set -gx PATH $BASE_PATH $EMACS_PATH $RASP_GCC $NPM_HOME $CARGO $GOPATH/bin $LOCALBIN $GOBIN $DOOMPATH $FLUTTER_PATH $DART_PATH
+
+function fish_title
+    true
+end
