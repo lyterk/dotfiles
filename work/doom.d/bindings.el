30d87d09bfa337390599ee87f9d7c9406c3b3faa
Kevin Lyter <kllyter@amazon.com>
2020-01-13 16:27:09 -0800

    Some notification stuff

--- work/doom.d/bindings.el
+++ work/doom.d/bindings.el
@@ -27,10 +27,10 @@
 
 (map! :leader
       (:prefix-map ("k" . "kevin")
-        :desc "Log out user" "o" #'logout
-        :desc "rot13 buffer" "r" #'toggle-rot13-mode
-        :desc "Ace-window" "w" #'ace-window
-        :desc "Query-replace" "r" #'anzu-query-replace
-        :desc "Query-replace RegExp" "R" #'anzu-query-replace-regexp
-        :desc "Yank current filename as org link" "f" #'copy-file-name
-        :desc "Yank current filename::linenum as org link" "l" #'copy-file-name-with-number))
+        :desc "Log out user"                                "o"  #'logout
+        :desc "rot13 buffer"                                "r"  #'toggle-rot13-mode
+        :desc "Ace-window"                                  "w"  #'ace-window
+        :desc "Query-replace"                               "r"  #'anzu-query-replace
+        :desc "Query-replace RegExp"                        "R"  #'anzu-query-replace-regexp
+        :desc "Yank current filename as org link"           "f"  #'copy-file-name
+        :desc "Yank current filename::linenum as org link"  "l"  #'copy-file-name-with-number))
