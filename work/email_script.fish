2420c400abd5bc8f26d40347043628d7a817b34e
Kevin Lyter <kllyter@amazon.com>
2020-07-21 13:56:02 -0700

    Fix up work email a bit

--- work/email_script.fish
+++ work/email_script.fish
@@ -2,13 +2,11 @@
 
 set nm /usr/bin/notmuch
 set me "kllyter"
-set boss "skipark" "aurelv"
+set boss "mansataa" "tompcase" "saindane"
 set director "joannste"
-set team "skipark" "foot" "djcoop" "styerp" "malav" "momallor"
-set relatives "vaggi" "soufiane" "bls" "kwillet"
+set team "weny" "nicwrin" "zhaoyic" "amyni" "varuojha" "adheep" "yjiaa"
 set domain "@amazon.com"
 set retrieval "Retrieval using the IMAP4 protocol failed for the following message"
-set password_notify "Project Austin Redshift Cluster - Monthly Automated Password Change"
 set deinbox -inbox -unread
 
 eval $nm new
@@ -32,10 +30,6 @@ for user in $team
     nm_tag +team "from:$user$domain OR to:$user$domain"
 end
 
-for user in relatives
-    nm_tag +distant_team "from:$user$domain OR to:$user$domain"
-end
-
 # More likely to be automated
 
 nm_tag +automated_cruft +trash +deleted -inbox -unread "Cron AND from:root@"
@@ -44,29 +38,15 @@ nm_tag +automated_cruft +trash +deleted -inbox -unread "$retrieval"
 
 nm_tag +dwp +trash -inbox -unread "from:dwp"
 
-nm_tag +datanet_monitor -inbox -unread "from:fba-analytics-routine"
-
-nm_tag +airflow_auto -inbox "from:fba-airflow"
-
-nm_tag +worthless_cheng -inbox -unread "from:fbaanalyticsdata"
-
-nm_tag +passwords -inbox $password_notify
-
-nm_tag +alarms -inbox "from:fba-analysis-alarms"
-
 nm_tag +tickets "from:remedy$domain OR (Ticket AND Correspondence)"
 
 nm_tag +permissions "from:permissions-notifier"
-
-nm_tag +trash -inbox -unread "Undeliverable: QA Flash for"
 
 nm_tag +sim from:"issues@prod.sim.a2z.com"
 
 # Interest lists and chatter
 
-set interest_lists "bdt-interest" "djangohackers" "dryad-interest" "emacs" "emr-interest" "etl-users" "hammerstone-interest" "motorbikes" "pythonhackers" "quicksight-interest" "redshift-interest" "apache-spark-interest" "ubuntu-users" "aaa-interest"
-
-set chatter_lists "seattle-chatter"
+set interest_lists "bdt-interest" "djangohackers" "dryad-interest" "emacs" "emr-interest" "etl-users" "hammerstone-interest" "motorbikes" "pythonhackers" "quicksight-interest" "redshift-interest" "apache-spark-interest" "ubuntu-users" "aaa-interest" "coe-interest"
 
 for list in $interest_lists
     nm_tag -inbox +$list +interest_list "to:$list$domain"
