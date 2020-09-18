e0bbc05ff7bca2f5d1b67a4855f3838e82ebc6b8
Kevin Lyter <kllyter@amazon.com>
2020-09-18 13:05:43 -0700

    Fix vpn script

--- work/scripts/vpn.sh
+++ work/scripts/vpn.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env zsh
 
-pin=$(pass Amzn/Midway_PIN)
+export pin=$(pass Amzn/Midway_PIN)
 read "token?Enter token: "
 
 vpn_echo_string="5\nkllyter\n$pin$token\ny"
 
-# echo $vpn_echo_string | /opt/cisco/anyconnect/bin/vpn -s connect sea-h-orca.amazon.com
+echo $vpn_echo_string | /opt/cisco/anyconnect/bin/vpn -s connect sea-h-orca.amazon.com
 
-mwinit_echo_string="$pin\n$token\n"
-echo $mwinit_echo_string | mwinit -o
+# mwinit_echo_string="$pin\n$token\n"
+# echo $mwinit_echo_string | mwinit -o
+
+./expect.sh
