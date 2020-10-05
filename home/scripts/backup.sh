2f7d6d274b4427b3f70024952e3edad2f456ec8d
Kevin Lyter <lyterk@sent.com>
2020-10-04 20:54:16 -0700

    Backup changes

--- home/scripts/backup.sh
+++ home/scripts/backup.sh
@@ -5,14 +5,13 @@ staging_dir=$HOME/.backup-staging
 mkdir -p $staging_dir
 
 encrypt_key=7BC4D154D6F8B0F15F5456851ABEC6FC8FFE77DC
-sign_key=CD3397AFBB9848841EB8A0574FAC48A86C1B43BB
-export SIGN_PASSPHRASE="netflix_another_life"
 
 duplicity --encrypt-key $encrypt_key \
     --exclude $HOME/.cache/ \
     --exclude $HOME/.gimp-2.8/ \
     --exclude $HOME/.gnupg/ \
     --exclude $HOME/.go/ \
+    --exclude $HOME/.m3/ \
     --exclude $HOME/.mozilla/ \
     --exclude $HOME/.password-store/ \
     --exclude $HOME/.pki/ \
@@ -24,4 +23,4 @@ duplicity --encrypt-key $encrypt_key \
     --exclude $HOME/Videos/ \
     $HOME file://$staging_dir
 
-rsync -avpz $staging_dir nuc:/mnt/toshiba/backups-asus/
+rsync -avpz $staging_dir nuc:/mnt/toshiba/backups-$(hostname)/
--- home/scripts/duplicity_backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export GPG_KEY=84302C86569002E023AF4B83DF0567A05B291E1C
-export GOOGLE_DRIVE_SETTINGS=~/.duplicity/credentials
-duplicity --exclude '/home/kev/.*' \
-          --encrypt-sign-key="$GPG_KEY" \
-          --log-file "/home/kev/.duplicity/info.log" \
-          /home/kev/ pydrive+gdocs://lyterk@gmail.com/backup
