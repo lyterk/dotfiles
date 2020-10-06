4ba8bdad3f86227800eda0fdd473cd0aec9be110
Kevin Lyter <kllyter@amazon.com>
2020-10-06 14:28:33 -0700

    Clean up fish config

--- work/fish/config.fish
+++ work/fish/config.fish
@@ -19,13 +19,6 @@ alias i=ipython
 alias python=python3
 alias pipi="pip3 install --user"
 
-alias mon 'psql -h fba-analysis.c5g1ihldokmx.us-east-1.redshift.amazonaws.com -p 8192 -d monster -U fba_analysis'
-alias monr 'psql -h fba-analysis.c5g1ihldokmx.us-east-1.redshift.amazonaws.com -p 8192 -d monster -U root'
-alias gol 'psql -h fba-analysis-goldman.db.amazon.com -p 8192 -d goldman -U root'
-alias wells 'psql -h fba-wells.c5g1ihldokmx.us-east-1.redshift.amazonaws.com -p 8192 -d wells -U root'
-alias ags 'psql -h agsanalytics01.cuv0wvsrttvh.us-east-1.redshift.amazonaws.com -p 8192 -d agsanalytics -U ags_root'
-alias fare 'psql -h fare-bi-rs1.cejlc1wjv9tl.us-east-1.redshift.amazonaws.com -p 8192 -d farebirs1 -U dw_virtdashboard_admin'
-alias wb "~/.local/share/sql_workbench/sqlworkbench.sh &"
 alias edae "emacs --daemon > ~/.emacs.d/log/'(date '+%m-%d-%y')'.log & "
 alias et "emacsclient -t"
 alias vipy "$VIRTUAL_ENV/bin/ipython"
@@ -35,22 +28,14 @@ alias python python3
 alias pip $HOME/.pyenv/shims/pip3
 alias csi "rlwrap csi"
 
-# for package in FBAFareCommon FBAFareMaestroServiceModel FBASparkSettingManager FBAFareMaestroUIWebsiteContent FBAFareMaestroUIService
-#     cd $package
-#     bb
-#     cd ..
-# end
-
 alias bnu "brazil ws create --name"
 alias bp "brazil ws use -p"
 alias bv "brazil ws use -vs"
 
 alias ssam "sudo /home/linuxbrew/.linuxbrew/bin/sam"
 
-alias pahvo 'psql -h pahvo.cakudculty6n.us-east-1.rds.amazonaws.com -p 5432 -d pahvo -U airflow_test_user'
-
-alias ppush "pass git push origin master"
-alias ppull "pass git pull --rebase origin master"
+alias ppush "pass git push origin mainline"
+alias ppull "pass git pull --rebase origin mainline"
 
 set -x SOUND_FOLDER "$HOME/Music"
 alias pat 'play $SOUND_FOLDER/right_answer.mp3 2> /dev/null'
@@ -63,9 +48,6 @@ set -x EDITOR "/usr/bin/emacsclient -t"
 
 set -x RUST_SRC_PATH $HOME/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src
 
-
-set -x JAVA_HOME /usr/lib/jvm/java-11-openjdk-amd64/bin
-set -x JDK_HOME /usr/lib/jvm/java-11-openjdk-amd64
 set -x NPM $HOME/.nvm/versions/node/v8.16.0/bin
 set -x LOCALBIN $HOME/.local/bin
 set -x NODEPATH $HOME/.nvm/versions/node/v12.2.0/bin
@@ -99,9 +81,6 @@ end
 
 function sync_dots
     rsync -a --cvs-exclude ~/dotfiles/ desk:~/dotfiles &
-    # rsync -a --cvs-exclude ~/dotfiles/ d:~/dotfiles &
-    # rsync -a --cvs-exclude ~/dotfiles/ e:~/dotfiles &
-    rsync -a --cvs-exclude ~/dotfiles/ f:~/dotfiles &
 end
 
 set -x sdet /workspace/sde_integ/src/SDEIntegrationTests
