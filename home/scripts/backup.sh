6dfd9352f19419a3f02c3b44ab76a6b0f5f368d2
Kevin Lyter <lyterk@sent.com>
2022-03-07 17:55:53 -0800

    Update backup script in case I use it
    Add all of my current filters, they ought to come in handy

--- home/scripts/backup.sh
+++ home/scripts/backup.sh
@@ -1,27 +1,68 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-staging_dir=$HOME/.backup-staging/
+staging_dir=$HOME/.backups/
 mkdir -p $staging_dir
 
 encrypt_key=7BC4D154D6F8B0F15F5456851ABEC6FC8FFE77DC
 
 duplicity --encrypt-key $encrypt_key \
-    --exclude $HOME/.cache/ \
-    --exclude $HOME/.gimp-2.8/ \
-    --exclude $HOME/.gnupg/ \
-    --exclude $HOME/.go/ \
-    --exclude $HOME/.m3/ \
-    --exclude $HOME/.mozilla/ \
-    --exclude $HOME/.password-store/ \
-    --exclude $HOME/.pki/ \
-    --exclude $HOME/.ssh/ \
-    --exclude $HOME/.urxvt/ \
-    --exclude $HOME/.var/ \
-    --exclude $HOME/.vim/ \
-    --exclude $staging_dir \
-    --exclude $HOME/Videos/ \
-    --exclude $staging_dir/ \
+    --exclude $HOME/.adobe \
+    --exclude $HOME/.aqbanking \
+    --exclude $HOME/.audacity-data \
+    --exclude $HOME/.aws \
+    --exclude $HOME/.backups \
+    --exclude $HOME/.boot \
+    --exclude $HOME/.cabal \
+    --exclude $HOME/.cache \
+    --exclude $HOME/.cargo \
+    --exclude $HOME/.config \
+    --exclude $HOME/.dbus \
+    --exclude $HOME/.doom.d \
+    --exclude $HOME/.elfeed \
+    --exclude $HOME/.emacs.d \
+    --exclude $HOME/.factorio \
+    --exclude $HOME/.fltk \
+    --exclude $HOME/.ghc \
+    --exclude $HOME/.ghcup \
+    --exclude $HOME/.gnupg \
+    --exclude $HOME/.go \
+    --exclude $HOME/.gphoto \
+    --exclude $HOME/.hex \
+    --exclude $HOME/.ipython \
+    --exclude $HOME/.java \
+    --exclude $HOME/.keychain \
+    --exclude $HOME/.local \
+    --exclude $HOME/.m2 \
+    --exclude $HOME/.mail \
+    --exclude $HOME/.mix \
+    --exclude $HOME/.mozilla \
+    --exclude $HOME/.npm \
+    --exclude $HOME/.nvm \
+    --exclude $HOME/.old_doom.d \
+    --exclude $HOME/.old_old_doom.d \
+    --exclude $HOME/.opam \
+    --exclude $HOME/.password-store \
+    --exclude $HOME/.pip \
+    --exclude $HOME/.pki \
+    --exclude $HOME/.rustup \
+    --exclude $HOME/.screenlayout \
+    --exclude $HOME/.slime \
+    --exclude $HOME/.ssh \
+    --exclude $HOME/.ssr \
+    --exclude $HOME/.stack \
+    --exclude $HOME/.steam \
+    --exclude $HOME/.tox \
+    --exclude $HOME/.urxvt \
+    --exclude $HOME/.var \
+    --exclude $HOME/.vcpkg \
+    --exclude $HOME/.vim \
+    --exclude $HOME/.vscode-oss \
+    --exclude $HOME/.w3m \
+    --exclude $HOME/.wine \
+    --exclude $HOME/Music \
+    --exclude $HOME/Videos \
+    --exclude $HOME/go
     $HOME file://$staging_dir
 
-rsync -avpz $staging_dir backup:/mnt/toshiba/backups/$(hostname)
+rsync --archive --perms --compress $staging_dir backup:/backup/
