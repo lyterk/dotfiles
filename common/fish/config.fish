c8695e5996aa3f0c4418e57d81c0de3e95068ad4
Kevin Lyter <lyterk@sent.com>
2023-02-25 21:27:36 -0800

    Ocaml addition

--- common/fish/config.fish
+++ common/fish/config.fish
@@ -38,3 +38,6 @@ end
 if test -f ~/.keychain/(hostname)-fish
     source ~/.keychain/(hostname)-fish
 end
+
+# opam configuration
+source /home/lyterk/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
